88121911e63a4ee438e4f82473a4e039c42ef0e7
0xLott <llott1710@gmail.com>
2023-04-11 09:52:06 -0300

    Métodos adicionar e retirar da lista

--- codigo/business/Cliente.java
+++ codigo/business/Cliente.java
@@ -12,11 +12,21 @@ Cliente {
     }
 
     public void adicionarNaLista(Serie serie){
-        this.listaParaVer.add(serie);
+        if (PlataformaStreamimg.getSeries().contains(serie))
+            this.listaParaVer.add(serie);
+    };
+
+    public void retirarDaLista(String nomeSerie){
+        Series[] series = new Series[listaParaVer.size()];
+        series = listaParaVer.allElements(series)
+
+        for (Serie serie : series){
+            if (serie.getNome().equals(nomeSerie))
+                listaParaVer.remove(nomeSerie)
+        }
     };
 
 
-    public void retirarDaLista(String nomeSerie);
     public Lista<Serie> void filtrarPorGenero(String genero);
     public Lista<Serie> void filtrarPorIdioma(String idioma);
     public Lista<Serie> filtrarPorQtdEpisodios(int quantEpisodios);

211c2acdcec5267a771e264002fb09c23640af6a
0xLott <llott1710@gmail.com>
2023-04-11 09:42:08 -0300

    Adicionadas assinaturas de métodos

--- codigo/business/Cliente.java
+++ codigo/business/Cliente.java
@@ -4,8 +4,22 @@ Cliente {
     private Lista<Serie> listaParaVer;
     private Lista<Serie> listaJaVistas;
 
-    public void adicionarNaLista(Serie serie);
-    public void retirarDaLista();
-    public void filtrarPorGenero();
+    public Cliente (nomeDeUsuario, senha, listaParaVer, listaJaVistas){
+        this.nomeDeUsuario = nomeDeUsuario;
+        this.senha = senha;
+        this.listaParaVer = listaParaVer
+        this.listaJaVistas = listaJaVistas;
+    }
+
+    public void adicionarNaLista(Serie serie){
+        this.listaParaVer.add(serie);
+    };
+
+
+    public void retirarDaLista(String nomeSerie);
+    public Lista<Serie> void filtrarPorGenero(String genero);
+    public Lista<Serie> void filtrarPorIdioma(String idioma);
+    public Lista<Serie> filtrarPorQtdEpisodios(int quantEpisodios);
+    public void registrarAudiencia(Serie serie);
 
 }

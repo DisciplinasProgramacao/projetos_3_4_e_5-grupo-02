a5836f22e33fa1b19227243c03635afa3dafe745
André Rodrigues <mrnexusrider@hotmail.com>
2023-04-11 10:05:15 -0300

    Correcao classe Cliente

--- codigo/test/ClienteTest.java
+++ codigo/test/ClienteTest.java
@@ -1,9 +1,15 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 public class ClienteTest {
     
-    
+    @Test
+    public void demo(){
+        assertTrue(true);
+    }
     
 
 

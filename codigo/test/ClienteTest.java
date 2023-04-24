3a656e01b2c91153307410e774b73d4ddf3200b5
André Rodrigues <mrnexusrider@hotmail.com>
2023-04-24 16:16:56 -0300

    Adicionados 2 métodos Cliente Test

--- codigo/test/ClienteTest.java
+++ codigo/test/ClienteTest.java
@@ -15,22 +15,47 @@ public class ClienteTest {
         assertTrue(1 + 1 == 2);
     }
 
-    // @BeforeEach
-    // public void setUp() {
-    // Serie s = new Serie("Breaking Bad", "Drama", "Portuguese", 25);
-    // Cliente c = new Cliente("xX_TesterCraft_Xx", "teste123");
-    // }
-
-    @Test
-    public void test() {
-        Lista<Serie> l = new Lista<>();
-
+    @BeforeEach
+    public void setUp() {
         Serie s = new Serie("Breaking Bad", "Drama", "Portuguese", 25);
         Cliente c = new Cliente("xX_TesterCraft_Xx", "teste123");
+    }
+
+    @Test
+    public void filtrarPorGeneroTest() {
+        Lista<Serie> l = new Lista<>();
 
         c.adicionarNaLista(s); // Adiciona a série na lista de series do cliente
 
         l = c.filtrarPorGenero("Drama"); // A lista de séries recebe a serie filtrada pelo genero
+
+        Serie vetorSeries[] = new Serie[10]; // Cria um vetor de séries
+        l.allElements(vetorSeries); // Adiciona no vetorSeries todas as séries contidas na lista 'l'
+
+        assertEquals("Breaking Bad", vetorSeries[0]);
+    }
+
+    @Test
+    public void filtrarPorEpisodiosTest() {
+        Lista<Serie> l = new Lista<>();
+
+        c.adicionarNaLista(s); // Adiciona a série na lista de series do cliente
+
+        l = c.filtrarPorQtdEpisodios(25); // A lista de séries recebe a serie filtrada pelo genero
+
+        Serie vetorSeries[] = new Serie[10]; // Cria um vetor de séries
+        l.allElements(vetorSeries); // Adiciona no vetorSeries todas as séries contidas na lista 'l'
+
+        assertEquals("Breaking Bad", vetorSeries[0]);
+    }
+
+    @Test
+    public void filtrarPorIdiomaTest() {
+        Lista<Serie> l = new Lista<>();
+
+        c.adicionarNaLista(s); // Adiciona a série na lista de series do cliente
+
+        l = c.filtrarPorIdioma("Portuguese"); // A lista de séries recebe a serie filtrada pelo genero
 
         Serie vetorSeries[] = new Serie[10]; // Cria um vetor de séries
         l.allElements(vetorSeries); // Adiciona no vetorSeries todas as séries contidas na lista 'l'

82890e842cca5a7028f3779efe488762f66c3099
0xLott <llott1710@gmail.com>
2023-04-24 22:18:31 -0300

    Atualizado método carregarSeries()

--- codigo/app/App.java
+++ codigo/app/App.java
@@ -1,13 +1,15 @@
 package app;
 
 import business.Cliente;
+import business.Midia;
 import business.PlataformaStreaming;
 import java.io.FileNotFoundException;
+import java.text.ParseException;
 
 public class App {
-    public static void main(String[] args) throws FileNotFoundException{
-        PlataformaStreaming plataforma = new PlataformaStreaming("CatFlix", new Cliente("Lott", "Lott123"));
+    public static void main(String[] args) throws FileNotFoundException {
+        PlataformaStreaming plataforma = new PlataformaStreaming("Flin TeX", new Cliente("Lott", "Lott123"));
 
-        plataforma.carregarSeries();
+       plataforma.carregarSeries();
     }
 }

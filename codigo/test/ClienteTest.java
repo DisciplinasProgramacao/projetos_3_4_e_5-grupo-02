0d4e670b02a14aaca4e65e1cb72b993ccb80d305
André Rodrigues <mrnexusrider@hotmail.com>
2023-04-26 18:00:03 -0300

    Adicionado dois métodos do Cliente Test

--- codigo/test/ClienteTest.java
+++ codigo/test/ClienteTest.java
@@ -23,37 +23,40 @@ public class ClienteTest {
 
     @BeforeEach
     public void setUp() {
-    s  = new Serie("Breaking Bad", "Drama", "Portuguese", new Date(), 25);
-    c  = new Cliente("xX_TesterCraft_Xx", "teste123");
+        s = new Serie("Breaking Bad", "Drama", "Portuguese", new Date(), 25);
+        c = new Cliente("xX_TesterCraft_Xx", "teste123");
 
-    Lista<Serie> l = new Lista<>(); // Cria uma lista secundária de Séries
+        Lista<Serie> l = new Lista<>(); // Cria uma lista secundária de Séries
 
-    c.adicionarNaLista(s); // Adiciona a série na lista de series do cliente
+        c.adicionarNaLista(s); // Adiciona a série na lista de series do cliente
 
-    int x = Integer.parseInt("1");
+        int x = Integer.parseInt("1");
 
     }
 
     @Test
     public void filtrarPorGeneroTest() {
-        Serie s1  = new Serie("Breaking Good", "Suspense", "Portuguese", new Date(), 25);
+        Serie s1 = new Serie("Breaking Good", "Suspense", "Portuguese", new Date(), 25);
         c.adicionarNaLista(s1);
         l = c.filtrarPorGenero("Drama"); // A lista de séries recebe a serie filtrada pelo genero
-
 
         assertEquals(1, l.size());
     }
 
     @Test
-    public void removerElementosDaLista() {
+    public void adicionarSerieNaListaTest() {
+
+        assertEquals(1, c.tamanhoListaParaVer());
+
+    }
+
+    @Test // O teste está errado pois não está adicionando elemento na lista, logo não há
+          // o que remover
+    public void removerSerieDaListaTest() {
 
         c.retirarDaLista("Breaking Bad");
 
-        assertEquals();
-
-
-
+        assertEquals(0, c.tamanhoListaParaVer());
     }
-    
 
 }

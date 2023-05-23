b6ed3b58d234123d94cdd94a838015711fe9ddab
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-05-23 02:05:41 -0300

    Add files via upload

---
+++ codigo/test/FilmeTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import business.Filme;
+
+class FilmeTest {
+
+	public static Filme filme;
+	public static Filme filme1;
+	public static Filme filme2;
+	
+	@BeforeEach
+	void iniciaTeste() {
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ codigo/test/SerieTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import business.Serie;
+
+class SerieTest {
+
+	public static Serie serie;
+	public static Serie serie1;
+	public static Serie serie2;
+	
+	@BeforeEach
+	void iniciaTeste() {
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

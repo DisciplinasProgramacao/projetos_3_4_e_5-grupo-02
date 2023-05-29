8f543081361556eebb51d17d133d1b934d9302b2
André Rodrigues <mrnexusrider@hotmail.com>
2023-05-29 14:21:24 -0300

    Update teste Avaliacao

--- codigo/test/AvaliacaoTest.java
+++ codigo/test/AvaliacaoTest.java
@@ -1,7 +1,28 @@
 package test;
 
-public class AvaliacaoTest {
-    
+import org.junit.Test;
 
-    
+import business.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class AvaliacaoTest {
+
+    Serie s;
+    Cliente c;
+
+    @BeforeEach
+    public void setUp() {
+        s = new Serie("Breaking Bad", "Drama", "Português", new Date(), 25);
+        c = new Cliente("xX_TesterCraft_Xx", "10", "teste123");
+    }
+
+    @Test
+    public void testarAvaliacao() {
+        Avaliacao a = new Avaliacao(c, 4);
+    }
+
 }

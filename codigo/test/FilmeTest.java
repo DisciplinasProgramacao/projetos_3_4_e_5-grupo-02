b278958e5a2ddaf34b6aa79fae90418624394507
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-05-30 23:42:49 -0300

    Update FilmeTest.java

--- codigo/test/FilmeTest.java
+++ codigo/test/FilmeTest.java
@@ -2,25 +2,48 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import business.Avaliacao;
+import business.Cliente;
 import business.Filme;
 
 class FilmeTest {
 
-	public static Filme filme;
-	public static Filme filme1;
-	public static Filme filme2;
+	public static Filme f;
+	public static Cliente c;
+	public static Cliente c1;
 	
 	@BeforeEach
 	void iniciaTeste() {
-		
+		c = new Cliente("Joao", "Joao123", "senha");
+		c1 = new Cliente("Jose", "Jose123", "password");
+		f = new Filme("Gato de Botas 2", "Animação", "Portugûes", new Date(), 6000);
 	}
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCriarAvaliacao() {
+		String s = "Excelente filme";
+		int nota = 5;
+		f.criarAvaliacao(c, nota, s);
+		
+		assertEquals(5, f.getQtdAva().get(0).getNota());
+	}
+	
+	@Test 
+	void testMediaAvaliacoes() {
+		String s = "Excelente filme";
+		int nota = 5;
+		f.criarAvaliacao(c, nota, s);
+		
+		String s1 = "Filme bom";
+		int nota1 = 3;
+		f.criarAvaliacao(c1, nota1, s1);
+			
+		assertEquals(4, f.mediaAvaliacoes());
 	}
 
 }

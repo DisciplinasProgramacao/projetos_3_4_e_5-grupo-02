fbd0b443362a96389bf7aa26a8f2179d502193c5
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-05-30 23:43:37 -0300

    Update SerieTest.java

--- codigo/test/SerieTest.java
+++ codigo/test/SerieTest.java
@@ -2,25 +2,48 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import business.Avaliacao;
+import business.Cliente;
 import business.Serie;
 
 class SerieTest {
 
-	public static Serie serie;
-	public static Serie serie1;
-	public static Serie serie2;
+	public static Serie s;
+	public static Cliente c;
+	public static Cliente c1;
 	
 	@BeforeEach
 	void iniciaTeste() {
-		
+		c = new Cliente("Joao", "Joao123", "senha");
+		c1 = new Cliente("Jose", "Jose123", "password");
+		s = new Serie("Wandinha", "Comédia", "Portugûes", new Date(), 6000);
 	}
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCriarAvaliacao() {
+		String comentario = "Serie legal, bem sombria";
+		int nota = 4;
+		s.criarAvaliacao(c, nota, comentario);
+		
+		assertEquals(4, s.getQtdAva().get(0).getNota());
+	}
+	
+	@Test 
+	void testMediaAvaliacoes() {
+		String comentario = "Serie legal, bem sombria";
+		int nota = 4;
+		s.criarAvaliacao(c, nota, comentario);
+		
+		String comentario1 = "Nao gostei";
+		int nota1 = 1;
+		s.criarAvaliacao(c1, nota1, comentario1);
+			
+		assertEquals(2.5, s.mediaAvaliacoes());
 	}
 
 }

c09c71fe19c9a04d683581699fbf552aa75cfc15
Letícia Lott <103859841+0xLott@users.noreply.github.com>
2023-05-30 23:40:15 -0300

    Atualizado Javadoc

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"business"},{"l":"business.exceptions"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"business"},{"l":"business.exceptions"},{"l":"business.interfaces"}];updateSearchResults();

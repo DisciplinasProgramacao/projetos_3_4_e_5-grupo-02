416d461ba1662c4a0acaba1ee7343b9379fec416
Carlos Emanuel <carlos.olvr32@gmail.com>
2023-06-11 14:30:50 -0300

    add menu

--- codigo/app/App.java
+++ codigo/app/App.java
@@ -14,22 +14,40 @@ public class App {
     public static void main(String[] args) {
         /*Variaveis */
         Scanner read = new Scanner(System.in);
-        PlataformaStreaming plataforma = new PlataformaStreaming("Xam OBH", new Cliente("John Doe", "Jd123", "psswd789"));
-        int op = 0;
+        PlataformaStreaming plataform = new PlataformaStreaming("Xam OBH", new Cliente("John Doe", "Jd123", "psswd789"));
+        int option = 0;
 
         /*Carregar dados */
         try{
-            plataforma.carregarAudiencia();
-            plataforma.carregarClientes();
-            plataforma.carregarFilmes();
-            plataforma.carregarSeries();
+            plataform.carregarAudiencia();
+            plataform.carregarClientes();
+            plataform.carregarFilmes();
+            plataform.carregarSeries();
 
-            System.out.println("Olá, " + plataforma.getClienteAtual().getNomeUsuario() + "! Bem-vindo(a) a Xam OBH!\n");
+            System.out.printf("Olá, " + plataform.getClienteAtual().getNomeUsuario() + "! Bem-vindo(a) a demonstração do Xam OBH!\n");
             
         } catch(FileNotFoundException e) {
-            System.out.println("Erro ao entrar na plataforma");
-        } finally {
+            System.out.printf("Erro ao entrar na plataforma");
             read.close();
+        }
+
+        while(option != 99) {
+            System.out.printf("Usuário: %s\n", plataform.getClienteAtual().getNomeUsuario());
+            System.out.printf("Menu: \n");
+            System.out.printf("""
+                                Escolha uma operação:
+                                --- Gerenciar Clientes ---
+                                1. Fazer login com outro usuário.
+                                2. Cadastrar novo cliente.
+                                --- Gerenciar Mídias ---
+                                3. Cadastrar nova série.
+                                4. Assistir série.
+                                --- Outros ---
+                                98. Demonstrar outras funções.
+                                99. Salvar e sair.
+                                """);
+            System.out.printf("Opção: ");
+            option = read.nextInt();
         }
     }     
 }

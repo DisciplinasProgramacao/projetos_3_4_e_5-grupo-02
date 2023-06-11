addcf858fa6c2faa40359839e3f5987d246eaa24
Carlos Emanuel <carlos.olvr32@gmail.com>
2023-06-11 16:31:40 -0300

    add método cadastrar nova mídia

--- codigo/app/App.java
+++ codigo/app/App.java
@@ -50,11 +50,84 @@ public class App {
 
         try{
             plat.adicionarCliente(userName, userId, userPassword);
-            System.out.printf("Usuário cadastrado com sucesso.");
+            System.out.printf("Usuário cadastrado com sucesso.\n");
         } catch(ElementoJaExisteException e) {
             System.out.printf(e.getMessage());
         }
     }
+    
+    /*Método estatico para cadastrar nova midia */
+    public static void cadastrarMidia(PlataformaStreaming plat) {
+        String midiaName, midiaGenre, midiaIdiom, midiaRelease, midiaType;
+        int midiaId;
+
+        System.out.printf("---------- Cadastrar Midia ----------\n");
+
+        System.out.printf("Nome: ");
+        midiaName = read.nextLine();
+
+        System.out.printf("ID: ");
+        midiaId = read.nextInt();
+        read.nextLine();
+
+        System.out.printf("Gêneros disponíveis: Comédia, Ação, Terror, Drama, Romance, Aventura, Animação, Suspense\n");
+        System.out.printf("Gênero: ");
+        midiaGenre = read.nextLine();
+
+        System.out.printf("Idiomos disponíveis: Português, Inglês, Esperanto, Romeno\n");
+        System.out.printf("Idioma: ");
+        midiaIdiom = read.nextLine();
+
+        System.out.printf("Data de lançamento(aaaa-mm-dd): ");
+        midiaRelease = read.nextLine();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date midiaReleaseDate = null;
+
+        try{
+            midiaReleaseDate = dateFormat.parse(midiaRelease);
+        } catch(ParseException e) {
+            System.out.printf("Formato de data invalido\n");
+        }
+
+        System.out.printf("""
+                            Selecione o tipo de mídia: 
+                            a. Série.
+                            b. Filme.
+                            """);
+        
+        midiaType = read.nextLine();
+
+        switch(midiaType) {
+            case "a" -> {
+                System.out.printf("Quantidade de episodios: ");
+                int serieQtdEp = read.nextInt();
+                read.nextLine();
+
+                try{
+                    plat.adicionarSerie(midiaId, new Serie(midiaName, midiaGenre, midiaIdiom, midiaReleaseDate, serieQtdEp));
+                    System.out.printf("Série cadastrada com sucesso!\n");
+                }catch(ElementoJaExisteException | NullPointerException e){
+                    System.out.println(e.getMessage());
+                }
+            } 
+
+            case "b" -> {
+                System.out.printf("Duração do filme (min): ");
+                int filmLength = read.nextInt();
+                read.nextLine();
+
+                try{
+                    plat.adicionarFilme(midiaId, new Filme(midiaName, midiaGenre, midiaIdiom, midiaReleaseDate, filmLength));
+                    System.out.printf("Filme cadastrado com sucesso!\n");
+                }catch(ElementoJaExisteException | NullPointerException e){
+                    System.out.println(e.getMessage());
+                }
+            } 
+        }
+    }
+
+
     public static void main(String[] args) {
         /*Variaveis */
         PlataformaStreaming plataform = new PlataformaStreaming("Xam OBH", new Cliente("John Doe", "Jd123", "psswd456"));
@@ -83,8 +156,8 @@ public class App {
                                 1. Fazer login com outro usuário.
                                 2. Cadastrar novo cliente.
                                 --- Gerenciar Mídias ---
-                                3. Cadastrar nova série.
-                                4. Assistir série.
+                                3. Cadastrar nova mídia.
+                                4. Assistir mídia.
                                 --- Outros ---
                                 98. Demonstrar outras funções.
                                 99. Salvar e sair.
@@ -102,7 +175,7 @@ public class App {
                     cadastrarCliente(plataform);
                     break;
                 case 3:
-                    System.out.printf("Opção 03 selecionada\n");
+                    cadastrarMidia(plataform);
                     break;
                 case 4:
                     System.out.printf("Opção 04 selecionada\n");

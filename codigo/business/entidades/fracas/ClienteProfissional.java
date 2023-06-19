cc6cf5625e68bbd612031c9d68ef41c6eff54389
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-06-19 06:18:13 -0300

    Update ClienteProfissional.java

--- codigo/business/entidades/fracas/ClienteProfissional.java
+++ codigo/business/entidades/fracas/ClienteProfissional.java
@@ -1,13 +1,21 @@
 package business.entidades.fracas;
-
 import business.entidades.Cliente;
+import business.entidades.Midia;
+import business.interfaces.ICliente;
 
-public class ClienteProfissional extends Cliente {
-	
-	private String profissao;
-	
-	public ClienteProfissional(String nomeDeUsuario, String id, String senha, String profissao) {
-		super(nomeDeUsuario, id, senha);
-		this.profissao = profissao;
+public class ClienteProfissional implements ICliente {
+
+    /**
+     * Chama o método criarAvaliacao(Cliente, int, String) de mídia.
+     *
+     * @param midia Mídia a ser avaliada
+     * @param avaliador Cliente que está avaliando
+     * @param nota  Nota a ser atribuída à mídia avaliada
+     * @param comentario Comentário a ser atribuído na mídia avaliada
+     * @throws IllegalStateException Caso este cliente já tenha avaliado a mesma mídia anteriormente
+     */
+	@Override
+	public void avaliarMidia(Midia midia, Cliente avaliador, int nota, String comentario) throws IllegalStateException {
+		midia.criarAvaliacao(avaliador, nota, comentario);
 	}
 }
